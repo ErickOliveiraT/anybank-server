600cee21142fa5da7bab8d232cc0f2dd3c643866
Érick de Oliveira Teixeira <38773400+ErickOliveiraT@users.noreply.github.com>
2024-10-02 21:01:49 -0300

    Deposit when seed accounts

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -1,10 +1,15 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AccountService } from "src/services/account.service";
 import { AccountOpenDTO } from "src/dtos/account_open.dto";
+import { DepositService } from "src/services/deposit.service";
+import { faker } from "@faker-js/faker";
 
 @Controller('accounts')
 export class AccountController {
-    constructor (private readonly accountService: AccountService) {}
+    constructor (
+        private readonly accountService: AccountService,
+        private readonly depositService: DepositService,
+    ) {}
     
     @Post()
     async createAccount(@Body() body: AccountOpenDTO) {
@@ -14,7 +19,18 @@ export class AccountController {
 
     @Post('seed')
     async generateAccount() {
-        return await this.accountService.generateAccounts();
+        const accounts = await this.accountService.generateAccounts();
+        const depositPromises = [];
+        for (const account of accounts) {
+            depositPromises.push(
+                this.depositService.depositToAccount(
+                    account.id,
+                    Number(faker.number.float({min: 0, max: 10000}).toFixed(2))
+                )
+            );
+        }
+        await Promise.all(depositPromises);
+        return accounts;
     }
 
     
