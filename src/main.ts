93bd0e729be0505e1c009c0350e5a8333f65667a
Érick de Oliveira Teixeira <38773400+ErickOliveiraT@users.noreply.github.com>
2024-10-15 23:39:12 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:54225', // Permitir apenas essa origem (Flutter)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
